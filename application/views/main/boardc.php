c7991aabc261df7f0cc099dac405f2b8b21f1df6
adriana <apatriciarojas@ucundinamarca.edu.co>
2023-01-25 20:13:57 -0500

    se agrega otra ventana sin el inicio de seion

---
+++ application/views/main/boardc.php
@@ -0,0 +1,492 @@
+<?php
+error_reporting(0);
+
+/**
+ *************************************************************************
+ *************************************************************************
+ Creado por:                 	Juan Carlos Escobar Baquero
+ Correo electr�nico:          	jcescobarba@gmail.com
+ Creaci�n:                    	27/02/2018
+ Modificaci�n:                	2019/11/06
+ Prop�sito:						P�gina Web.
+ *************************************************************************
+ *************************************************************************
+ ******************** BOGOT� COLOMBIA 2018 *******************************
+ */
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+
+
+<!-- This page CSS -->
+
+
+
+<!-- ============================================================== -->
+<!-- Start Page Content -->
+<!-- ============================================================== -->
+
+<!-- ============================================================== -->
+<!-- Inicio totales generales -->
+<!-- ============================================================== -->
+
+<!-- Librerias para gr�ficos -->
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+<script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+
+<h1>
+	<i class=""></i>MI QR
+</h1>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!--Puedes descargar el script e incluirlo de manera local si así prefieres-->
+	<script src="https://unpkg.com/qrious@4.0.2/dist/qrious.js"></script>
+	<title>Generar códigos QR - By Parzibyte</title>
+</head>
+
+<body>
+	<img alt="Código QR" id="codigo">
+	<script>
+		new QRious({
+			element: document.querySelector("#codigo"),
+			value: "Hola me llamo <?= $NOMBRES . " " . $APELLIDOS ?> y mi ruta es la siguiente (Dar Click)  https://www.google.com/maps/d/edit?hl=es-419&mid=1KG9s036NJj38yru9w06Muvuy3mx9Cb8&ll=4.68123780007098%2C-74.10017604511188&z=14 /n Cualquier duda o inquietud llamar al 3219779439", // La URL o el texto
+			size: 200,
+			backgroundAlpha: 0, // 0 para fondo transparente
+			foreground: "#0391d1", // Color del QR
+			level: "M", // Puede ser L,M,Q y H (L es el de menor nivel, H el mayor)
+		});
+	</script>
+</body>
+
+</html>
+<!DOCTYPE html>
+<!-- saved from url=(0037)https://me-qr.com/entry/vcard/C3XVK5F -->
+<html lang="en">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>
+		VCard </title>
+	<link rel="icon" type="image/png" href="https://me-qr.com/favicon.ico">
+	<link rel="alternate" type="application/rss+xml" href="https://me-qr.com/feed.rss">
+	<meta name="description" content="Easy QR code maker. Create QR code in two clicks: 👉 upload your link, 👉 generate QR and 👍 download it!">
+	<meta name="keywords" content="qr code generator, free qr code, create qr code online, qr barcode, free qr code, barcode maker">
+	<link href="./VCard _files/material-kit.min.css" rel="stylesheet">
+	<link href="./VCard _files/base-style.css" rel="stylesheet">
+
+	<link rel="apple-touch-icon" sizes="76x76" href="https://me-qr.com/favicon.ico">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport">
+	<meta property="og:type" content="website">
+	<meta property="og:locale:alternate" content="en">
+	<meta property="og:locale:alternate" content="es">
+	<meta property="og:locale:alternate" content="ms">
+	<meta property="og:locale:alternate" content="pt">
+	<meta property="og:locale:alternate" content="ru">
+	<meta property="og:locale:alternate" content="vi">
+	<meta property="og:locale:alternate" content="fr">
+	<meta property="og:locale:alternate" content="th">
+	<meta property="og:locale:alternate" content="de">
+	<meta property="og:locale:alternate" content="ar">
+	<meta property="og:locale:alternate" content="id">
+	<meta property="og:title" content="VCard" adriana="" sierra="">
+	<meta property="og:locale" content="en/">
+	<meta property="og:description" content="Easy" qr="" code="" maker.="" create="" in="" two="" clicks:="" 👉="" upload="" your="" link,="" generate="" and="" 👍="" download="" it!="">
+	<meta property="og:image" content="https://me-qr.com/static/pages/logo/logo-2.jpg/">
+</head>
+
+<body class="landing-page sidebar-collapse">
+	<style type="text/css">
+		.circle {
+			width: 1967px;
+			height: 1943px;
+			left: -850px;
+			top: -358px;
+			background-color: #F6F8FA;
+			opacity: .4;
+			position: absolute;
+			border-radius: 100%;
+			z-index: -1;
+		}
+
+		.circle-wrapper {
+			position: absolute;
+			top: -80px;
+			left: 0px;
+			bottom: 0px;
+			right: 0px;
+			overflow: hidden;
+		}
+
+		.content {
+			padding: 80px 80px 0;
+			background: rgba(230, 242, 255, 0.5);
+			position: relative;
+			z-index: 1;
+			flex: 1 1 1010px;
+		}
+
+		.top-block {
+			height: 294px;
+			background: #222222;
+			border-radius: 4px;
+		}
+
+		.top-block img {
+			object-fit: cover;
+		}
+
+		.top-image {
+			max-width: 282px;
+			height: auto;
+		}
+
+		.user-name {
+			font-family: 'Roboto';
+			font-style: normal;
+			font-weight: 500;
+			font-size: 40px;
+			line-height: 47px;
+			color: #FFFFFF;
+			margin-top: 0;
+		}
+
+		.name-block {
+			padding-top: 92px;
+			padding-left: 60px;
+		}
+
+		.user-work {
+			font-family: 'Roboto';
+			font-style: normal;
+			font-weight: 400;
+			font-size: 20px;
+			line-height: 23px;
+			color: #EEEEEE;
+			margin-top: 0;
+		}
+
+		.header-collapse {
+			border: 1px solid #D0D0D0 !important;
+			border-radius: 8px !important;
+		}
+
+		.header-collapse a {
+			padding: 14px 21px;
+			color: black !important;
+		}
+
+		.header-collapse a:hover {
+			color: black !important;
+		}
+
+		.action-button {
+			background: #7B2DC9;
+			border-radius: 4px;
+			padding: 12px 0px;
+			color: white;
+			border: 1px solid #7B2DC9;
+			text-align: center;
+			font-weight: 500;
+			font-size: 15px;
+			line-height: 18px;
+		}
+
+		.action-button:hover {
+			color: #7B2DC9;
+			background: white;
+		}
+
+		.collapse-body {
+			position: relative;
+			top: -3px;
+			border-radius: 0 0 8px 8px;
+			border: 1px solid #D0D0D0 !important;
+			border-top: none !important;
+		}
+
+		.download-btn {
+			position: relative;
+			z-index: 1;
+			color: #7B2DC9;
+			background-color: transparent;
+			font-weight: 600;
+			font-size: 15px;
+			line-height: 18px;
+		}
+
+		.download-btn:hover {
+			color: white;
+			background-color: #7B2DC9;
+		}
+
+		.icon-collapse {
+			width: 20px;
+			margin-right: 8px;
+		}
+
+		.text-truncate {
+			max-width: 275px;
+		}
+
+		@media (max-width: 991px) {
+			.content {
+				padding: 20px;
+			}
+
+			.top-block {
+				min-width: 334px;
+				height: 174px;
+			}
+
+			.top-image {
+				width: 122px;
+			}
+
+			.name-block {
+				padding-top: 52px;
+				padding-left: 20px;
+			}
+
+			.user-name {
+				font-size: 24px;
+				line-height: 28px;
+			}
+
+			.user-work {
+				font-weight: 300;
+				font-size: 12px;
+				line-height: 18px;
+			}
+
+			.header-collapse a {
+				padding: 14px;
+				color: black !important;
+			}
+		}
+
+		@media (min-width: 575px) {
+			.text-truncate {
+				max-width: 420px;
+			}
+		}
+
+		@media (min-width: 768px) {
+			.text-truncate {
+				max-width: 595px;
+			}
+		}
+
+		@media (min-width: 992px) {
+			.text-truncate {
+				max-width: 720px;
+			}
+		}
+
+		@media (min-width: 1200px) {
+			.text-truncate {
+				max-width: 895px;
+			}
+		}
+	</style>
+	<div class="container-fluid d-flex justify-content-center">
+		<div class="container d-flex justify-content-center">
+			<div class="content d-flex flex-column">
+				<div class="circle-wrapper">
+					<div class="circle"></div>
+				</div>
+				<div class="top-block d-flex">
+					<img class="top-image" src="<?= base_url() ?>assets/images/logo.png">
+					<div class="name-block">
+						<h1 class="user-name">Hola me llamo <?= $NOMBRES . " " . $APELLIDOS ?> </h1>
+						<h2 class="user-work"></h2>
+						<h2 class="user-work">
+							https://asociacionrecicladoresCartagena.org/</h2>
+					</div>
+				</div>
+				<ul id="accordion" role="tablist" aria-multiselectable="true" class="card-collapse p-0" style="list-style: none">
+					<li class="card-plain pt-4">
+						<div class="card-header header-collapse p-0" role="tab" id="headingEight">
+							<a class="" data-toggle="collapse" data-parent="#accordion" href="https://me-qr.com/entry/vcard/C3XVK5F#workPhone" aria-expanded="true" aria-controls="workPhone">
+								<span class="collapse-title font-weight-normal">
+									<i class="fa fa-volume-control-phone" aria-hidden="true"></i>
+									Telefono de la Empresa
+								</span>
+								<i class="material-icons">
+									<img style="float: right;" width="24" height="24">
+								</i>
+							</a>
+						</div>
+						<div id="workPhone" class="bg-white collapse-body collapse show" role="tabpanel" aria-labelledby="headingEight" style="">
+							<div class="card-body d-flex flex-column p-4">
+								<p class="font-weight-normal mb-4"> <?PHP echo $telefono; ?>
+									<a href="https://api.whatsapp.com/send/?phone=57<?PHP echo $telefono ?>&text=Hola&type=phone_number&app_absent=0" target="_blank">
+										<img src="<?= base_url() ?>assets/images/logo-wasap.png" width="50" height="50">
+									</a>
+								</p>
+								<td>
+
+								</td>
+								<a class="action-button" href="tel:<?PHP echo $telefono; ?>">Llamar Empresa</a>
+							</div>
+						</div>
+					</li>
+				</ul>
+				<ul id="accordion" role="tablist" aria-multiselectable="true" class="card-collapse p-0" style="list-style: none">
+					<li class="card-plain pt-3">
+						<div class="card-header header-collapse p-0" role="tab" id="headingEight">
+							<a class="" data-toggle="collapse" data-parent="#accordion" href="https://me-qr.com/entry/vcard/C3XVK5F#phone" aria-expanded="true" aria-controls="phone">
+								<i class="fa fa-mobile" aria-hidden="true"></i>
+								Telefono Personal
+								</span>
+								<i class="material-icons">
+									<img style="float: right;" width="24" height="24">
+								</i>
+							</a>
+						</div>
+						<div id="workPhone" class="bg-white collapse-body collapse show" role="tabpanel" aria-labelledby="headingEight" style="">
+							<div class="card-body d-flex flex-column p-4">
+								<p class="font-weight-normal mb-4"> <?PHP echo $telefonoreciclador; ?>
+									<a href="https://api.whatsapp.com/send/?phone=57<?PHP echo $telefonoreciclador ?>&text=Hola&type=phone_number&app_absent=0" target="_blank">
+										<img src="<?= base_url() ?>assets/images/logo-wasap.png" width="50" height="50">
+									</a>
+								</p>
+								<td>
+
+								</td>
+								<a class="action-button" href="tel:<?PHP echo $telefonoreciclador; ?>">Llamar Personal</a>
+							</div>
+						</div>
+					</li>
+				</ul>
+				<ul id="accordion" role="tablist" aria-multiselectable="true" class="card-collapse p-0" style="list-style: none">
+					<li class="card-plain pt-3">
+						<div class="card-header header-collapse p-0" role="tab" id="headingEight">
+							<a class="" data-toggle="collapse" data-parent="#accordion" href="https://me-qr.com/entry/vcard/C3XVK5F#website" aria-expanded="true" aria-controls="website">
+								<i class="fa fa-map" aria-hidden="true"></i>
+								Ruta del Reciclador
+								</span>
+
+								<i class="material-icons">
+									<img style="float: right;" width="24" height="24">
+								</i>
+							</a>
+						</div>
+						<div id="website" class="bg-white collapse-body collapse show" role="tabpanel" aria-labelledby="headingEight" style="">
+							<div class="card-body d-flex flex-column p-4">
+								<?PHP echo $ruta?>
+							</div>
+				</ul>
+				<ul id="accordion" role="tablist" aria-multiselectable="true" class="card-collapse p-0" style="list-style: none">
+					<li class="card-plain pt-3">
+						<div class="card-header header-collapse p-0" role="tab" id="headingEight">
+							<a class="" data-toggle="collapse" data-parent="#accordion" href="https://me-qr.com/entry/vcard/C3XVK5F#location" aria-expanded="true" aria-controls="location">
+								<span class="collapse-title font-weight-normal">
+									<i class="fa fa-map-marker" aria-hidden="true"></i>
+									Ubicacion Actual
+								</span>
+								<i class="material-icons">
+									<img style="float: right;" width="24" height="24">
+								</i>
+							</a>
+						</div>
+						<style type="text/css">
+							#map {
+								height: 400px;
+								width: 100%;
+							}
+						</style>
+					</li>
+					<div id="location" class="bg-white collapse-body collapse show" role="tabpanel" aria-labelledby="location" style="">
+						<div class="card-body d-flex flex-column p-4">
+
+							<body>
+								<div id="map"></div>
+								<script>
+									function initMap() {
+										// Obtiene la ubicación actual del usuario
+										if (navigator.geolocation) {
+											navigator.geolocation.getCurrentPosition(function(position) {
+												var pos = {
+													lat: position.coords.latitude,
+													lng: position.coords.longitude
+												};
+												// Muestra el mapa en la ubicación actual del usuario
+												var map = new google.maps.Map(document.getElementById('map'), {
+													zoom: 16,
+													center: pos
+												});
+												// Añade un marcador en la ubicación actual del usuario
+												var marker = new google.maps.Marker({
+													position: pos,
+													map: map
+												});
+											});
+										}
+									}
+								</script>
+								<script async defer src="https://maps.googleapis.com/maps/api/js?key=&callback=initMap">
+								</script>
+							</body>
+						</div>
+					</div>
+					</li>
+				</ul>
+				<ul id="accordion" role="tablist" aria-multiselectable="true" class="card-collapse p-0" style="list-style: none">
+					<li class="card-plain pt-3">
+						<div class="card-header header-collapse p-0" role="tab" id="headingEight">
+							<a class="" data-toggle="collapse" data-parent="#accordion" href="https://me-qr.com/entry/vcard/C3XVK5F#email" aria-expanded="true" aria-controls="email">
+								<span class="collapse-title font-weight-normal">
+									<i class="fa fa-envelope" aria-hidden="true"></i>
+									Email
+								</span>
+								<i class="material-icons">
+									<img style="float: right;" width="24" height="24">
+								</i>
+							</a>
+						</div>
+						<div id="email" class="bg-white collapse-body collapse show" role="tabpanel" aria-labelledby="email" style="">
+							<div class="card-body d-flex flex-column p-4">
+								<p class="font-weight-normal mb-4"><?PHP echo $correo; ?></p>
+								<a class="action-button" href="mailto:<?PHP echo $correo; ?>">Enviar Correo
+								</a>
+							</div>
+						</div>
+					</li>
+				</ul>
+				<a href="https://cdn.me-qr.com/vcf/vcards/339196.vcf" class="mb-5 mt-3 action-button download-btn">Descargar datos</a>
+			</div>
+		</div>
+	</div>
+	<script defer="" src="./VCard _files/bootstrap-material-design.min.js.descarga" type="text/javascript"></script>
+	<script src="./VCard _files/jquery.min.js.descarga" type="text/javascript"></script>
+
+	<script async="" src="./VCard _files/js"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+
+		gtag('js', new Date());
+
+		gtag('config', 'G-LNNBBTRGEJ');
+	</script>
+	<link rel="stylesheet" type="text/css" href="./VCard _files/css">
+
+
+
+
+	<script defer="" src="./VCard _files/lazy.js.descarga"></script>
+	<script defer="" src="./VCard _files/vaafb692b2aea4879b33c060e79fe94621666317369993" integrity="sha512-0ahDYl866UMhKuYcW078ScMalXqtFJggm7TmlUtp0UlD4eQk0Ixfnm5ykXKvGJNFjLMoortdseTfsRT8oCfgGA==" data-cf-beacon="{&quot;rayId&quot;:&quot;76cfa9504d52f887&quot;,&quot;version&quot;:&quot;2022.11.0&quot;,&quot;r&quot;:1,&quot;token&quot;:&quot;ba9386ef69374dbc9ca25bc62eb77e0a&quot;,&quot;si&quot;:100}" crossorigin="anonymous"></script>
+
+
+</body>
+
+</html>
